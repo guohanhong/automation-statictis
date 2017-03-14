3ad2a620598d5f83f8d4e7337c15a6bde0a63164
guohanhong <guohanhong@askingdata.com>
2017-03-14 16:32:08 +0800

    init automation-statictis

---
+++ src/main/java/org/automation/statictis/App.java
@@ -0,0 +1,13 @@
+package org.automation.statictis;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
